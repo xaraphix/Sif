067b4ccdb58dbb6aba19911e14ad1a6ff23bd50c
Suyash Singh <suyash.x@gmail.com>
2021-08-18 01:34:55 +0530

    adds e2e for log replication

--- test/raft_e2e_test.go
+++ test/raft_e2e_test.go
@@ -4,13 +4,15 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/xaraphix/Sif/internal/raft"
+	"github.com/xaraphix/Sif/internal/raft/protos"
 	. "github.com/xaraphix/Sif/test/testbed_setup"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 var _ = Describe("Sif Raft Consensus E2E", func() {
 	Context("Multiple raft nodes", func() {
 		When("A node starts an election", func() {
-			FSpecify("The first node to start the election must win it if there is no other candidate and network errors", func() {
+			Specify("The first node to start the election must win it if there is no other candidate and network errors", func() {
 
 				node1, node2, node3, node4, node5 := Setup5FollowerNodes()
 
@@ -18,9 +20,9 @@ var _ = Describe("Sif Raft Consensus E2E", func() {
 				lId := node1.Id
 				for {
 					if node2.CurrentLeader == lId &&
-					node3.CurrentLeader == lId &&
-					node4.CurrentLeader == lId &&
-					node5.CurrentLeader == lId 	{
+						node3.CurrentLeader == lId &&
+						node4.CurrentLeader == lId &&
+						node5.CurrentLeader == lId {
 						break
 					}
 				}
@@ -56,9 +58,9 @@ var _ = Describe("Sif Raft Consensus E2E", func() {
 				lId = node3.Id
 				for {
 					if node2.CurrentLeader == lId &&
-					node1.CurrentLeader == lId &&
-					node4.CurrentLeader == lId &&
-					node5.CurrentLeader == lId 	{
+						node1.CurrentLeader == lId &&
+						node4.CurrentLeader == lId &&
+						node5.CurrentLeader == lId {
 						break
 					}
 				}
@@ -87,6 +89,52 @@ var _ = Describe("Sif Raft Consensus E2E", func() {
 				raft.DestructRaftNode(node4)
 				raft.DestructRaftNode(node5)
 
+			})
+
+			FSpecify("If a node becomes a leader and has logs more recent than other nodes, it should replicate the logs to peers", func() {
+
+				node1, node2, node3, node4, node5 := Setup5FollowerNodes()
+
+				node1.Logs = append(node1.Logs, &protos.Log{Term: 1, Message: &structpb.Struct{
+					Fields: map[string]*structpb.Value{
+						"A": {
+							Kind: &structpb.Value_StringValue{
+								StringValue: "B",
+							},
+					},}}},
+					&protos.Log{Term: 1, Message: &structpb.Struct{
+					Fields: map[string]*structpb.Value{
+						"C": {
+							Kind: &structpb.Value_StringValue{
+								StringValue: "B",
+							},
+					},}}},
+				)
+
+				node1.LeaderHeartbeatMonitor.Start(node1)
+				lId := node1.Id
+
+				for {
+					if node2.CurrentLeader == lId &&
+						node3.CurrentLeader == lId &&
+						node4.CurrentLeader == lId &&
+						node5.CurrentLeader == lId && {
+						break
+					}
+				}
+
+
+				Expect(len(node2.Logs)).To(Equal(2))
+				Expect(len(node3.Logs)).To(Equal(2))
+				Expect(len(node4.Logs)).To(Equal(2))
+				Expect(len(node5.Logs)).To(Equal(2))
+
+
+				raft.DestructRaftNode(node1)
+				raft.DestructRaftNode(node2)
+				raft.DestructRaftNode(node3)
+				raft.DestructRaftNode(node4)
+				raft.DestructRaftNode(node5)
 			})
 		})
 	})
